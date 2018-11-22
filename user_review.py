7a95201af4635031f8ab662c1a3829a63e7bbbbf
Nella Zabrina <nellazabrinapramata@yahoo.co.id>
2018-11-22 12:47:18 +0700

    fixing put and use standardize message

--- user_review.py
+++ user_review.py
@@ -8,14 +8,20 @@ from peewee import IntegrityError
 class UserReviewHandler(Resource):
     def get(self, id=None):
         if request.method == 'GET':
-            model = UserReview()
-            results = model.select().where(id = id)
-            for result in results:
-                if (id == result['id']):
-                    return result.as_json(), 200
-                return "user_review not found", 404
+            if (id==None):
+                return None, 400
+            else:
+                results = UserReview().select().where(UserReview.id == id)
+                for result in results:
+                    if (id == result.id):
+                        return result.as_json(), 200
+                return {
+                    "message" : "user_review not found"
+                }, 404
         else:
-            return "method not allowed", 405
+            return {
+                "message" : "method not allowed"
+            }, 405
 
     def post(self, id=None):
         if request.method == 'POST':
@@ -43,38 +49,60 @@ class UserReviewHandler(Resource):
                     "message": "Order with id {} already exists".format(args["order_id"])
                 }, 400
         else:
-            return "method not allowed", 405
+            return {
+                "message" : "method not allowed"
+            }, 405
 
     def put(self, id=None):
         if request.method == 'PUT':
-            model = UserReview()
-            parser = reqparse.RequestParser()
-            parser.add_argument("order_id")
-            parser.add_argument("product_id")
-            parser.add_argument("user_id")
-            parser.add_argument("rating")
-            parser.add_argument("review")
-            args = parser.parse_args()
-            results = model.select()
-            for result in results:
-                if (id == result.id):
-                    updated = UserReview.update(rating = args["rating"], review = args["review"]).where(id == id)
-                    print(updated)
-                    return updated.as_json(), 200                
-            try:
-                data = {
-                    "order_id" : int(args["order_id"]),
-                    "product_id" : int(args["product_id"]),
-                    "user_id" : args["user_id"],
-                    "rating" : float(args["rating"]),
-                    "review" : args["review"]
-                }
-                created = UserReview.create(**data)
-                return created.as_json(), 201
-            except ValueError:
-                return None, 400
+            if (id==None):
+                return None
+            else:
+                parser = reqparse.RequestParser()
+                parser.add_argument("order_id")
+                parser.add_argument("product_id")
+                parser.add_argument("user_id")
+                parser.add_argument("rating")
+                parser.add_argument("review")
+                args = parser.parse_args()            
+                try:
+                    data = {
+                        "order_id" : int(args["order_id"]),
+                        "product_id" : int(args["product_id"]),
+                        "user_id" : args["user_id"],
+                        "rating" : float(args["rating"]),
+                        "review" : args["review"]
+                    }
+                    query  = UserReview().select().where(UserReview.id == id)
+                    if (len(query) == 0):
+                        return {
+                            "message" : "user_review not found"
+                        }, 404                        
+                    if ((query[0].order_id) == data["order_id"] and (query[0].user_id) == data["user_id"] and (query[0].product_id) == data["product_id"]):
+                        UserReview().update(**data).where(UserReview.id == id).execute()
+                        return query[0].as_json(), 200
+                    else:
+                        raise IntegrityError
+                except ValueError:
+                    return None, 400
+                except IntegrityError:
+                    query  = UserReview().select().where(UserReview.id == id)
+                    if ((query[0].user_id) != data["user_id"]):
+                        return {
+                            "message": "id user_review {} is not owned by {}".format(id, args["user_id"])
+                        }, 400
+                    elif ((query[0].order_id) != data["order_id"]):
+                        return {
+                            "message": "id user_review {} doesn't match with order_id {}".format(id, args["order_id"])
+                        }, 400
+                    elif ((query[0].product_id) != data["product_id"]):
+                        return {
+                            "message": "id user_review {} doesn't match with product_id {}".format(id, args["product_id"])
+                        }, 400       
         else:
-            return "method not allowed", 405
+            return {
+                "message" : "method not allowed"
+            }, 405
 
     def delete(self, id=None):
         if request.method == 'DELETE':
@@ -83,7 +111,13 @@ class UserReviewHandler(Resource):
             for result in results:
                 if (id == result.id):
                     result.delete_instance(recursive=False)
-                    return "{} is deleted".format(id), 200
-            return "The requested resource is no longer available at the server", 410
+                    return {
+                        "message" : "{} is deleted".format(id)
+                    }, 200
+            return {
+                "message" : "The requested resource is no longer available at the server"
+            }, 410
         else:
-            return "method not allowed", 405
+            return {
+                "message" : "method not allowed"
+            }, 405
